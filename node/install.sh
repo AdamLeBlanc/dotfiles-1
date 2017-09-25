3a390e1a3bfcca3109db5a783a67479607408da6
Adam LeBlanc <leblanca37@gmail.com>
2017-09-25 09:58:23 -0300

    Setup nvm and yarn. Install some global packages

--- node/install.sh
+++ node/install.sh
@@ -2,3 +2,13 @@ if test ! $(which spoof)
 then
   sudo npm install spoof -g
 fi
+
+if test ! $(which nvm)
+then
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.4/install.sh | bash
+fi
+
+nvm install lts/*
+nvm install node
+nvm alias default node
+yarn global add http-server create-react-app @angular-cli yo
