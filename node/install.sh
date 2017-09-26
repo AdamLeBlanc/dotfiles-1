2fb0314f8395a911270722bb52192838f42fa3b6
Adam LeBlanc <leblanca37@gmail.com>
2017-09-26 13:35:04 -0300

    Fixed nvm, back in :sunglasses:

--- node/install.sh
+++ node/install.sh
@@ -8,3 +8,8 @@ if test ! $(which nvm)
 then
   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.4/install.sh | bash
 fi
+source $NVM_DIR/nvm.sh
+nvm install lts/*
+nvm install node
+nvm alias default node
+yarn global add http-server create-react-app @angular/cli  yo
