5a6964e5559f164550655edaff3884dafb971b0f
Adam LeBlanc <leblanca37@gmail.com>
2017-09-26 13:30:24 -0300

    automatic setup of theme

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -6,3 +6,6 @@ export ZSH=$HOME/.oh-my-zsh
 if [ ! -d $ZSH ]; then
 	git clone https://github.com/robbyrussell/oh-my-zsh.git $ZSH
 fi
+
+
+curl https://raw.githubusercontent.com/caiogondim/bullet-train.zsh/master/bullet-train.zsh-theme > $ZSH/custom/bullet-train.zsh-theme
