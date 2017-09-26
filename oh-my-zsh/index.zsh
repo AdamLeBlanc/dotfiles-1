5ce804b3a51d4fd519ee011691a5abd014213a7a
Adam LeBlanc <leblanca37@gmail.com>
2017-09-25 22:54:56 -0300

    Added contextual nvm type

--- oh-my-zsh/index.zsh
+++ oh-my-zsh/index.zsh
@@ -1,10 +1,37 @@
 # inspired by https://github.com/rafaeleyng/dotfiles/blob/master/oh-my-zsh/index.zsh
 export ZSH=~/.oh-my-zsh
 
-ZSH_THEME="agnoster"
+#ZSH_THEME="agnoster"
+# Download here https://github.com/caiogondim/bullet-train.zsh/blob/master/bullet-train.zsh-theme
+ZSH_THEME="bullet-train"
 DEFAULT_USER=$(whoami)
 
 plugins=(docker git yarn z)
 
 source $ZSH/oh-my-zsh.sh
 #source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+#
+
+prompt_nvm(){
+    local nvm_prompt
+    if [ -f package.json ]; then
+        if type nvm >/dev/null 2>&1; then
+            nvm_prompt=$(nvm current 2>/dev/null)
+            [[ "${nvm_prompt}x" == "x" ]] && return
+        elif type node >/dev/null 2>&1; then
+            nvm_prompt="$(node --version)"
+        else
+            return
+        fi
+        nvm_prompt=${nvm_prompt}
+        prompt_segment $BULLETTRAIN_NVM_BG $BULLETTRAIN_NVM_FG $BULLETTRAIN_NVM_PREFIX$nvm_prompt
+    else
+        return
+    fi
+}
+
+BULLETTRAIN_PROMPT_ORDER=(
+    dir
+    nvm
+    git
+)
